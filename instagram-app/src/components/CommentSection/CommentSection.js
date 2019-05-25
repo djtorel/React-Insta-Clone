c24c0fd00ea3d491e25b8c039c271b15a8a66a62
Dominic Torruellas <dom@domtorr.com>
2019-05-25 11:23:14 -0700

    initial components

---
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./CommentSection.css";
+
+const CommentSection = () => <div />;
+
+export default CommentSection;
---
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const PostContainer = props => <div />;
+
+export default PostContainer;
