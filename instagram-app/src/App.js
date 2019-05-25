aae0464e210a687e5eb63cefaddb9f4e8dc2493b
Dominic Torruellas <dom@domtorr.com>
2019-05-25 11:29:54 -0700

    basic initial skeleton of App.js

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
+
+import data from './dummy-data';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: data,
+    };
+  }
+
+  renderPosts = posts => posts.map(post => <PostContainer posts={post} />);
+
+  render() {
+    const {
+      state: { posts },
+    } = this;
+    return (
+      <div>
+        <SearchBar />
+        {this.renderPosts(posts)}
+      </div>
+    );
+  }
 }
 
 export default App;
