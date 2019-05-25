909004141213f3a6b6bc3a3c4a6505408d7a3036
Dominic Torruellas <dom@domtorr.com>
2019-05-25 15:47:02 -0700

    basic skeleton of SearchBar

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
-const SearchBar = props => <div />;
+const SearchBar = props => (
+  <header>
+    <div>
+      <div>Logo</div>
+      <div>Company Name</div>
+    </div>
+    <div>
+      <input name="" type="text" value="" />
+    </div>
+    <div>
+      <div>Compass</div>
+      <div>Heart</div>
+      <div>Profile Icon</div>
+    </div>
+  </header>
+);
 
 export default SearchBar;
