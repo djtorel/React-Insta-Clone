6182862a651f78642f76baede64b6a6c4ab72dd3
Dominic Torruellas <dom@domtorr.com>
2019-05-27 15:16:52 -0700

    basic styling for PostContainer

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -2,35 +2,83 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+import './PostContainer.css';
 import CommentSection from '../CommentSection/CommentSection';
 
-const Heart = styled.div.attrs({
-  className: `jam jam-heart`,
+const Container = styled.div.attrs({
+  className: `bg-gray-600 shadow-md mb-5 rounded p-2`,
 })``;
+
+const PostHeader = styled.section.attrs({
+  className: `bg-gray-400 rounded shadow-md mb-2`,
+})``;
+
+const AuthorSection = styled.div.attrs({
+  className: `flex items-center p-4`,
+})``;
+
+const AuthorImg = styled.img.attrs({
+  className: `h-10 rounded-full`,
+})``;
+
+const AuthorName = styled.span.attrs({
+  className: 'ml-3 text-lg',
+})`
+  font-family: Roboto;
+`;
+
+const PostImg = styled.img.attrs({
+  className: `w-full`,
+})``;
+
+const LikesSection = styled.div.attrs({
+  className: 'px-4 py-2',
+})``;
+
+const IconContainer = styled.div.attrs({
+  className: `flex items-center text-3xl mb-2`,
+})``;
+
+const Heart = styled.div.attrs({
+  className: `jam jam-heart mr-3`,
+})`
+  cursor: pointer;
+`;
 
 const MessageBubble = styled.div.attrs({
   className: `jam jam-message-alt`,
-})``;
+})`
+  cursor: pointer;
+`;
+
+const LikesContainer = styled.div.attrs({
+  className: `flex text-sm`,
+})`
+  font-family: Roboto;
+`;
 
 const PostContainer = ({
   post: { username, thumbnailUrl, imageUrl, likes, timestamp, comments },
 }) => (
-  <div>
-    <div>
-      <img src={thumbnailUrl} alt="" />
-      <div>{username}</div>
-    </div>
-    <img src={imageUrl} alt="" />
-    <div>
-      <Heart />
-      <MessageBubble />
-    </div>
-    <div>
-      <div>{likes}</div>
-      <div>likes</div>
-    </div>
+  <Container>
+    <PostHeader>
+      <AuthorSection>
+        <AuthorImg src={thumbnailUrl} alt="" />
+        <AuthorName>{username}</AuthorName>
+      </AuthorSection>
+      <PostImg src={imageUrl} alt="" />
+      <LikesSection>
+        <IconContainer>
+          <Heart />
+          <MessageBubble />
+        </IconContainer>
+        <LikesContainer>
+          <div>{likes} likes</div>
+        </LikesContainer>
+      </LikesSection>
+    </PostHeader>
     <CommentSection comments={comments} timestamp={timestamp} />
-  </div>
+  </Container>
 );
 
 PostContainer.propTypes = {
