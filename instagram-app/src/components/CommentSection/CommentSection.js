ac1745b6eb81e3931b0bba65a4ff882375fc250e
Dominic Torruellas <dom@domtorr.com>
2019-05-26 23:04:42 -0700

    added prop types and basic skeleton to CommentSection and PostContainer

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import "./CommentSection.css";
+import './CommentSection.css';
+import Comment from './Comment';
 
-const CommentSection = () => <div />;
+const CommentSection = ({ comments }) => {
+  const renderComments = () =>
+    comments.map(({ username, text }, i) => (
+      <Comment key={i} username={username} text={text} />
+    ));
+  return (
+    <div>
+      {renderComments()}
+      <div>time ago</div>
+      <div>
+        <input name="" value="" type="text" placeholder="Add a comment..." />
+        <div>Comment Icon</div>
+      </div>
+    </div>
+  );
+};
 
 export default CommentSection;
+
+CommentSection.propTypes = {
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      username: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+    }),
+  ),
+};
--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const PostContainer = props => <div />;
+import CommentSection from '../CommentSection/CommentSection';
+
+const PostContainer = ({
+  post: { username, thumbnailUrl, imageUrl, likes, timestamp, comments },
+}) => (
+  <div>
+    <div>
+      <div>Author Picture</div>
+      <div>Author Name</div>
+    </div>
+    <div>Picture</div>
+    <div>
+      <div>Heart</div>
+      <div>Quote Bubble</div>
+    </div>
+    <div>
+      <div>Number of Likes</div>
+      <div>likes</div>
+    </div>
+    <CommentSection comments={comments} />
+  </div>
+);
 
 export default PostContainer;
+
+PostContainer.propTypes = {
+  post: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    thumbnailUrl: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    likes: PropTypes.number.isRequired,
+    timestamp: PropTypes.string.isRequired,
+    comments: PropTypes.array.isRequired,
+  }),
+};
